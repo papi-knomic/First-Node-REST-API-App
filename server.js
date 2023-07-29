fb5bed555d63e82bed9b2739aab79ff39baf2b9c
Samson Moses <knomic10@gmail.com>
2023-07-29 11:55:06 +0100

    First Commit For my First Rest API with Node & Express

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const serveIndex = require('serve-index');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('Time: ', Date.now());
+    next();
+  });
+
+app.use('/request-type', (req, res, next) => {
+    console.log('Request type: ', req.method);
+    next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Successful response.');
+});
+
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
